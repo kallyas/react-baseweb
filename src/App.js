e1d00cff97b10346372fd30248c12523c41ecc85
Kally <kallyasmedia@gmail.com>
2020-10-04 14:36:36 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { LightTheme, BaseProvider } from "baseui";
 
 function App() {
+  const engine = new Styletron();
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
